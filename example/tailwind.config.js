5cca9891511467e191a49e434b49eba50c12cd60
git <asyarb@hawaii.edu>
2020-10-18 10:13:24 -1000

    fix: example

--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -1,6 +1,3 @@
-const plugin = require('tailwindcss/plugin')
-const { default: capsize } = require('capsize')
-
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
@@ -35,34 +32,5 @@ module.exports = {
     extend: {},
   },
   variants: {},
-  plugins: [
-    plugin(({ addUtilities, theme, e }) => {
-      const capsizeSets = theme('capsize')
-
-      capsizeSets.forEach((set) => {
-        set.fontSets.forEach((fontSet) => {
-          const styles = capsize({
-            fontSize: fontSet.fontSize,
-            leading: fontSet.leading,
-            fontMetrics: set.fontMetrics,
-          })
-          styles['&::before'] = styles['::before']
-          styles['&::after'] = styles['::after']
-          delete styles['::before']
-          delete styles['::after']
-
-          const className = e(`${set.fontFamily}-${fontSet.fontSize}`)
-
-          const utilities = {
-            [`.${className}`]: {
-              fontFamily: theme(`fontFamily.${set.fontFamily}`),
-              ...styles,
-            },
-          }
-
-          addUtilities(utilities, ['responsive'])
-        })
-      })
-    }),
-  ],
+  plugins: [require('@asyarb/tailwind-capsize')],
 }
